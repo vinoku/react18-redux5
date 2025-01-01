b1f94600ad159acf7830868a9a86d723dadda507
vinh <vinoku78@gmail.com>
2025-01-01 01:58:22 +0100

    feat: initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("app");
+createRoot(rootElement).render(<p>Hi.</p>);
---
+++ webpack.config.dev.js
@@ -0,0 +1,41 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  stats: "minimal",
+  devServer: {
+    historyApiFallback: true,
+    headers: { "Access-Control-Allow-Origin": "*" }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
